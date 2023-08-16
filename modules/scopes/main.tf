8b852304be1fa8a56a0c36f7e168c9e73ad0edfc
Morgan Atkins <morgan.atkins@waltlabs.io>
2023-08-16 13:52:23 +0100

    adding full scope support

--- modules/scopes/main.tf
+++ modules/scopes/main.tf
@@ -2,120 +2,222 @@
 resource "aquasec_application_scope" "scopes" {
     name        = var.scope.name
     description = var.scope.description
-
-    dynamic "categories" {
-      for_each = try(var.scope.categories, {})
-      content {
-        dynamic "artifacts" {
-          for_each = try(categories.value.artifacts,{})
-          content {
-            image {
-              # expression = "v1 && v2"
-              dynamic "variables" {
-              for_each = try(artifacts.value.image.variables,{})
-                content {
-                attribute = variables.value.attribute
-                value     = variables.value.value
-                }
-              }
-            }
-            function {
-              # expression = "v1 && v2"
-              dynamic "variables" {
-              for_each = try(artifacts.value.function.variables,{})
-                content {
-                attribute = ""
-                value     = ""
-                }
-              }
-            }
-            cf {
-              # expression = "v1 && v2"
-              dynamic "variables" {
-              for_each = try(artifacts.value.cf.variables,{})
-                content {
-                attribute = ""
-                value     = ""
-                }
-              }
+    categories {
+      # entity_scope {
+      #   expression = "v1"
+      #   variables {
+      #     attribute = "application_scope.name"
+      #     value     = var.scope.name
+      #     }
+      # }
+      artifacts {
+          image {
+            expression = try(var.scope.artifacts.image.expression,{})
+            dynamic "variables" {
+            for_each = try(var.scope.artifacts.image.variables,{})
+            content {
+              attribute = variables.value.attribute
+              value     = variables.value.value
             }
           }
         }
-      # dynamic "workloads" {
-      #  for_each = try(categories.value.workloads, {})
-      #   content {
-      #     os {
-      #       expression = "v1 && v2"
-      #       dynamic "variables" {
-      #         for_each = try(workloads.value.os.variables,{})
-      #           content {
-      #           attribute = ""
-      #           value     = ""
-      #           }
-      #       }
-      #     }
-      #     kubernetes {
-      #       expression = "v1 && v2"
-      #       dynamic "variables" {
-      #         for_each = try(workloads.value.kubernetes.variables,{})
-      #           content {
-      #           attribute = ""
-      #           value     = ""
-      #           }
-      #       }
-      #     }
-      #     cf {
-      #       expression = "v1 && v2"
-      #       dynamic "variables" {
-      #         for_each = try(workloads.value.cf.variables,{})
-      #           content {
-      #           attribute = ""
-      #           value     = ""
-      #           }
-      #       }
-      #     }
-      #   }
-      #   }
+          function {
+            expression = try(var.scope.artifacts.function.expression,null)
+            dynamic "variables" {
+            for_each = try(var.scope.artifacts.function.variables,{})
+            content {
+              attribute = variables.value.attribute
+              value     = variables.value.value
+            }
+          }
+        }
+          cf {
+            expression = try(var.scope.artifacts.cf.expression,null)
+            dynamic "variables" {
+            for_each = try(var.scope.artifacts.cf.variables,{})
+            content {
+              attribute = variables.value.attribute
+              value     = variables.value.value
+            }
+          }
+        }
+      }
 
-        # dynamic "infrastructure" {
-        # for_each = try(categories.value.infrastructure,{})
-        #   content {
-        #     kubernetes {
-        #       dynamic "variables" {
-        #       for_each = try(infrastructure.value.kubernetes.variables,{})
-        #         content {
-        #         attribute = ""
-        #         value     = ""
-        #         }
-        #       }
-        #     }
-        #     os {
-        #       dynamic "variables" {
-        #       for_each = try(infrastructure.value.os.variables,{})
-        #         content {
-        #         attribute = ""
-        #         value     = ""
-        #         }
-        #       }
-        #     }
-        #   }
-        # }
-        # dynamic "entity_scope" {
-        # for_each = try(categories.value.entity_scope, {})
-        #   content {
-        #     expression = "v1"
-        #     dynamic "variables" {
-        #       for_each = try(entity_scope.value.variables, {})
-        #         content {
-        #         attribute = ""
-        #         value     = ""
-        #         }
-        #     }
-        #   }
-        # }
+
+      
+      workloads {
+          kubernetes {
+            expression = try(var.scope.workloads.kubernetes.expression,{})
+            dynamic "variables" {
+            for_each = try(var.scope.workloads.kubernetes.variables,{})
+            content {
+              attribute = variables.value.attribute
+              value     = variables.value.value
+            }
+          }
+        }
+          os {
+            expression = try(var.scope.workloads.os.expression,null)
+            dynamic "variables" {
+            for_each = try(var.scope.workloads.os.variables,{})
+            content {
+              attribute = variables.value.attribute
+              value     = variables.value.value
+            }
+          }
+        }
+          cf {
+            expression = try(var.scope.workloads.cf.expression,null)
+            dynamic "variables" {
+            for_each = try(var.scope.workloads.cf.variables,{})
+            content {
+              attribute = variables.value.attribute
+              value     = variables.value.value
+            }
+          }
+        }
+      }
+
+
+      
+      infrastructure {
+          kubernetes {
+            expression = try(var.scope.infrastructure.kubernetes.expression,{})
+            dynamic "variables" {
+            for_each = try(var.scope.infrastructure.kubernetes.variables,{})
+            content {
+              attribute = variables.value.attribute
+              value     = variables.value.value
+            }
+          }
+        }
+          os {
+            expression = try(var.scope.infrastructure.os.expression,null)
+            dynamic "variables" {
+            for_each = try(var.scope.infrastructure.os.variables,{})
+            content {
+              attribute = variables.value.attribute
+              value     = variables.value.value
+            }
+          }
+        }
+      }
     }
   }
-}
+
+# output "the-scope" {
+#     value = try(var.scope, {})
+# }
+# output "scope-image" {
+#     value = try(var.scope.artifacts[0], {})
+# }
+# output "scope-image-expression" {
+#     value = var.scope.artifacts.image.expression
+# }
+# output "scope-artifacts" {
+#     value = try(var.scope.artifacts, {})
+# }
+
+              # function {
+              #   # expression = "v1 && v2"
+              #   dynamic "variables" {
+              #   for_each = try(artifacts.value.function.variables,{})
+              #     content {
+              #     attribute = ""
+              #     value     = ""
+              #     }
+              #   }
+              # }
+              # cf {
+              #   # expression = "v1 && v2"
+              #   dynamic "variables" {
+              #   for_each = try(artifacts.value.cf.variables,{})
+              #     content {
+              #     attribute = ""
+              #     value     = ""
+              #     }
+              #   }
+              # }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+        # dynamic "workloads" {
+        #  for_each = try(categories.value.workloads, {})
+        #   content {
+        #     os {
+        #       expression = "v1 && v2"
+        #       dynamic "variables" {
+        #         for_each = try(workloads.value.os.variables,{})
+        #           content {
+        #           attribute = ""
+        #           value     = ""
+        #           }
+        #       }
+        #     }
+        #     kubernetes {
+        #       expression = "v1 && v2"
+        #       dynamic "variables" {
+        #         for_each = try(workloads.value.kubernetes.variables,{})
+        #           content {
+        #           attribute = ""
+        #           value     = ""
+        #           }
+        #       }
+        #     }
+        #     cf {
+        #       expression = "v1 && v2"
+        #       dynamic "variables" {
+        #         for_each = try(workloads.value.cf.variables,{})
+        #           content {
+        #           attribute = ""
+        #           value     = ""
+        #           }
+        #       }
+        #     }
+        #   }
+        #   }
+
+          # dynamic "infrastructure" {
+          # for_each = try(categories.value.infrastructure,{})
+          #   content {
+          #     kubernetes {
+          #       dynamic "variables" {
+          #       for_each = try(infrastructure.value.kubernetes.variables,{})
+          #         content {
+          #         attribute = ""
+          #         value     = ""
+          #         }
+          #       }
+          #     }
+          #     os {
+          #       dynamic "variables" {
+          #       for_each = try(infrastructure.value.os.variables,{})
+          #         content {
+          #         attribute = ""
+          #         value     = ""
+          #         }
+          #       }
+          #     }
+          #   }
+          # }
+          # dynamic "entity_scope" {
+          # for_each = try(categories.value.entity_scope, {})
+          #   content {
+          #     expression = "v1"
+          #     dynamic "variables" {
+          #       for_each = try(entity_scope.value.variables, {})
+          #         content {
+          #         attribute = ""
+          #         value     = ""
+          #         }
+          #     }
+          #   }
+          # }
+
 
     # {} [templatefile("${path.module}/scopes/template/scopes.tf.tmpl", { 
     #   types = var.categories[*]
--- modules/scopes/output.tf
+++ modules/scopes/output.tf
@@ -1,16 +1,19 @@
 
-output "name" {
-  value = var.name
-}
-output "artifacts" {
-  value = try(var.scope.categories["artifacts"], {})
-}
-output "infrastructure" {
-  value = try(var.scope.categories["infrastructure"], {})
-}
-output "workloads" {
-  value = try(var.scope.categories["workloads"], {})
-}
-output "entity_scope" {
-  value = try(var.scope.categories["entity_scope"], {})
-}
+# output "name" {
+#   value = var.name
+# }
+# output "artifacts" {
+#   value = try(var.scope.categories["artifacts"], {})
+# }
+# output "infrastructure" {
+#   value = try(var.scope.categories["infrastructure"], {})
+# }
+# output "workloads" {
+#   value = try(var.scope.categories["workloads"], {})
+# }
+# output "entity_scope" {
+#   value = try(var.scope.categories["entity_scope"], {})
+# }
+# output "images" {
+#     value = try(var.scope.categories.images, {})
+# }
